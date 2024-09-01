d57641c26a3f342c01feceea418ea919c2cf3372
Noah Petrides <nopetrides@gmail.com>
2024-09-01 13:34:28 -0700

    Exploring Visual Studio
    Basic project with an extra line

---
+++ Lab1/GD12_1133_Lab1_Petrides_Noah/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GD12_1133_Lab1_Petrides_Noah
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Noah Petrides ENTA 1133");
+        }
+    }
+}
