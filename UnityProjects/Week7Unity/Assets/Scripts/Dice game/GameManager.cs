6019589f1b37924a9a0857a1406d9a93662e06f2
noahpetrides <nopetrides@gmail.com>
2024-10-18 15:59:55 -0700

    name adjustment, remove old code

--- UnityProjects/Week7Unity/Assets/Scripts/Dice game/GameManager.cs
+++ UnityProjects/Week7Unity/Assets/Scripts/Dice game/GameManager.cs
@@ -2,41 +2,20 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private Map gameMap;
+    private Map _gameMap;
 
     public void Start()
     {
         Debug.Log("GameManager Start");
 
-        gameMap = new Map();
+        _gameMap = new Map();
 
         Debug.Log("GameManager Map Created");
 
         StartGame();
-
-        Vector2 myVector = new Vector2(3, 4);
-        float size = myVector.magnitude;
-        Debug.Log($"This vector has a magnitude of {size}. That was easy!");
-
-        /*
-        System.Random rand = new System.Random();
-
-        int playerPos = rand.Next(0, gameMap.MapSize);
-        RoomBase current = gameMap.RoomByIndex(playerPos);
-
-        // example moving north, setting the current room from current.North
-        
-        if (inputDir == 0 )
-        {
-            if (current.North != null)
-            {
-                current = current.North;
-            }
-        }
-        */
     }
 
-    public void StartGame()
+    private void StartGame()
     {
         // Intro
         Debug.Log("Hello, World!");
